f3c8d2ef15b57f517b7e3582b9e07044b3df173c
jitendriya <jitendriya.yagati@gmail.com>
2012-05-14 11:27:56 +0530

    Update src/xstrcmp.c

--- src/xstrcmp.c
+++ src/xstrcmp.c
@@ -1,6 +1,6 @@
 xstrcmp(char *string1,char *string2)
 {
-	int index=0;
+	int index = 0;
 	while( string1[index] == string2[index] && string2[index] != '\0')
 	index++;
 	if(string1[index] == string2[index])
